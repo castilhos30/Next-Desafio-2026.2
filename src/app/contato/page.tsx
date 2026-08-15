82176eb2236c3c8101ed64f556445d4ed8c315f2
MarcosCesar <marcoscesarcz@gmail.com>
2026-08-15 16:03:43 -0300

    feat(contato): adiciona título na seção de contato

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -6,6 +6,9 @@ import { Localizacao } from "@/components/localizacao"
 export default function Contato() {
     return (
         <main className="min-h-screen bg-gradient-to-l to-[#B76E79] via-[#D28582] from-[#ED9D8C] bg-cover bg-center bg-fixed py-12 px-4 pt-36 md:px-8 flex flex-col items-center">
+            
+            <h1 className="font-cairo font-bold text-white text-4xl md:text-6xl py-12"> Agende seu Momento</h1>
+            
             <div className="w-full max-w-7xl grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-24 place-items-center justify-between">
                 <Envio />
                 <Redes></Redes>
